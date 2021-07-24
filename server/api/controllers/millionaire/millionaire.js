956fe60fbba9b3b1dd2793938e47c02080f2e747
Iliyan Vlahov <iliyan.vlahov@gmail.com>
2021-07-24 22:52:29 +0300

    WIP: some read streams progression

--- server/api/controllers/millionaire/millionaire.js
+++ server/api/controllers/millionaire/millionaire.js
@@ -1,9 +1,14 @@
 import MillionaireService from '../../services/millionaire';
+import moment from 'moment';
 
 export class MillionaireController {
   getBuySellPoints(req, res) {
     const { start, end } = req.query;
-    MillionaireService.FindEarliestPoints().then((r) => res.json(r));
+    const startTimeStamp = moment(start).unix();
+    const endTimeStamp = moment(end).unix();
+    MillionaireService.testingHighland(startTimeStamp, endTimeStamp).then((r) =>
+      res.json(r)
+    );
   }
 }
 export default new MillionaireController();
--- server/api/services/millionaire.js
+++ server/api/services/millionaire.js
@@ -1,10 +1,12 @@
 import l from '../../common/logger';
+import _ from 'highland';
+import * as path from 'path';
+import * as fs from 'fs';
 
 class MillionaireService {
-  FindEarliestPoints(slice = []) {
+  FindEarliestPoints = () => (point) => {
     const length = slice.length;
     // validate that we have atleast 2 points in time
-    console.log(length);
     if (length < 2) {
       // TODO dummy resolve
       return Promise.resolve({
@@ -50,6 +52,48 @@ class MillionaireService {
       buyPoint: { dateTime: '2021-07-22T08:54:58Z', price: 5 },
       sellPoint: { dateTime: '2021-07-22T10:54:58Z', price: 10 },
     };
+  };
+
+  testingHighland(startTimeStamp, endTimeStamp) {
+    // return Promise.resolve({
+    //   buyPoint: { dateTime: '2021-07-22T08:54:58Z', price: 5 },
+    //   sellPoint: { dateTime: '2021-07-22T10:54:58Z', price: 10 },
+    // });
+
+    const expectedRecords = endTimeStamp - startTimeStamp + 1;
+
+    return new Promise((resolve, reject) => {
+      const readStream = fs.createReadStream(
+        path.resolve(__dirname, '../../small.csv')
+      );
+
+      readStream.on('error', (err) => {
+        reject(err);
+      });
+
+      _(readStream)
+        .invoke('toString', ['utf8'])
+        .split()
+        .filter((line) => line)
+        .map((line) => line.split(','))
+        .filter(([timeStampString]) => {
+          const timeStamp = Number(timeStampString);
+          console.log(timeStamp);
+          return timeStamp >= startTimeStamp && timeStamp <= endTimeStamp;
+        })
+        .map((line) => {
+          return line;
+        })
+        .each((x) => console.log(x))
+        .done(() => {
+          console.log('done');
+          console.log('Expected: ', expectedRecords);
+          resolve({
+            buyPoint: { dateTime: '2021-07-22T08:54:58Z', price: 5 },
+            sellPoint: { dateTime: '2021-07-22T10:54:58Z', price: 10 },
+          });
+        });
+    });
   }
 }
 
